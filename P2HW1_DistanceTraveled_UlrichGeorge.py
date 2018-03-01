b1bba40f9a25f2e657821209072f23d4cc610f28
GeorgeU4 <36967463+GeorgeU4@users.noreply.github.com>
2018-03-01 13:59:17 -0500

    Add files via upload
    First submission

---
+++ P2HW1_DistanceTraveled_UlrichGeorge.py
@@ -0,0 +1,33 @@
+
+# CTI-110 
+# P2HW1 - Distance Traveled  
+# George Ulrich
+# 02/20/18
+#
+
+#A car is traveling at 70 miles per hour. Display the following:
+#The distance the car will travel in 6 hours.
+#The distance the car will travel in 10 hours.
+#The distance the car will travel in 15 hours.
+
+print("After 6 hours:")
+SPEED = 70
+time_1 = 6
+distanceAfter6 = SPEED * time_1
+
+print("The distance traveled in 6 hours is" , (distanceAfter6))
+#time_1/2/3 is an easy way of differentiating them and keeping them in order.
+print("After 10 hours:")
+SPEED = 70
+time_2 = 10
+distanceAfter10 = SPEED * time_2
+
+print("The distance traveled in 10 hours is" , (distanceAfter10))
+#Using SPEED more than once is not necessary as it is a constant.
+#However using SPEED this way helps comprehension.
+print("After 15 hours:")
+SPEED = 70
+time_3 = 15
+distanceAfter15 = SPEED * time_3
+
+print("The distance traveled in 15 hours is" , (distanceAfter15))
